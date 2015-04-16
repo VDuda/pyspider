4c1e39473b20aa5d789baaa2f806cff872af3aab
Volodimir Duda <volod2010@gmail.com>
2015-04-16 15:40:23 -0400

    Adding a new skeleton create Workflow UI unit tests

--- tests/test_webui.py
+++ tests/test_webui.py
@@ -507,13 +507,14 @@ class TestWebUI(unittest.TestCase):
         rv = self.app.get('/tasks')
         self.assertEqual(rv.status_code, 502)
 
-    def test_debug_stateonload(self):
-        '''
-        Test if buttons are correctly loaded
-        '''
-        rv = self.app.get('/debug/test')
-        self.assertIn('<li data-id="tab-html" class="disabled">', rv.data)
-        self.assertIn('<li data-id="tab-web" class="disabled">', rv.data)
-        self.assertIn('<li data-id="J-enable-css-selector-helper" class="disabled">', rv.data)
-        self.assertIn('<li data-id="tab-follows" class="disabled">', rv.data)
+    def test_index_create_button(self):
+        rv = self.app.get('/')
+        self.assertEqual(rv.status_code, 200)
+        rv = self.app.get('/create')
+        self.assertEqual(rv.status_code, 200)
 
+    def test_index_create_prompt(self):
+        rv = self.app.get('/create')
+        self.assertIn(b'Title', rv.data)
+        self.assertIn(b'Group', rv.data)
+        self.assertIn(b'Target Website', rv.data)
